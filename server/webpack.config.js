a8eda966c37f78e34ff9df359026e9111d3e1e9d
EmptyEXOT <sadullaev.alim@mail.ru>
2022-11-01 08:52:56 +0300

    webpack init

---
+++ server/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    target: 'web',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './dist/index.html'),
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        open: true
+    },
+}
