43706fd75270050004d1272ebee7c835718cfb12
Alim <sadullaev.alim@mail.ru>
2022-11-06 18:13:00 +0300

    nickname has been added

--- server/src2/services/UserService.js
+++ server/src2/services/UserService.js
@@ -6,13 +6,13 @@ const tokenService = require('./TokenService');
 const UserDTO = require('../dtos/UserDTO')
 
 class UserService {
-    async registration(email, password) {
+    async registration(nickname, email, password) {
         const candidate = await userModel.findOne({email}); //Поиск юзера с переданным мейлом
         if (candidate) throw new Error('user with such email already exist'); //Если он есть, то бросаем ошибку
         const hashPassword = await bcrypt.hash(password, 7); //если нет - хешируем переданный пароль
         const activationLink = uuid.v4(); // создаем активационную ссылку
 
-        const user = await userModel.create({email, password: hashPassword, activationLink}); //созаем юзера
+        const user = await userModel.create({nickname, email, password: hashPassword, activationLink}); //созаем юзера
         await mailService.sendActivationLink(email, `${process.env.API_URL}api/activate/${activationLink}`); //отправляем активационную ссылку
 
         const userDTO = new UserDTO(user); //создаем объект, который будет передан как payload
